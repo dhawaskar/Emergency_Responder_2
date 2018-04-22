656d8f9ee357e46b6776b01d19c0dd7dc1fa2519
Michael Condon <mico0399@colorado.edu>
2018-04-21 20:24:03 -0600

    Added location marker as blue dot. Set location to be side door of math building in the map.

--- app/src/main/java/com/example/sandesh/speedometer/MapVisualization.java
+++ app/src/main/java/com/example/sandesh/speedometer/MapVisualization.java
@@ -2,12 +2,20 @@ package com.example.sandesh.speedometer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MapVisualization extends AppCompatActivity {
+    int sideDoorwayXCoordinate = 950;
+    int sideDoorwayYCoordinate = 620;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_visualization);
-    }
+
+        ImageView responderLocationImageView = findViewById(R.id.responder_location_image_view);
+        responderLocationImageView.setImageResource(R.drawable.responder_location);
+        responderLocationImageView.setTranslationX(sideDoorwayXCoordinate);
+        responderLocationImageView.setTranslationY(sideDoorwayYCoordinate);
+        }
 }
